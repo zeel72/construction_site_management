6020231779605fb35ced1fdb77d191cc83391f71
zeel moradiya <zeelmj272@gmail.com>
2026-08-15 19:42:59 +0530

    Fix middleware import in partyRoutes

--- server/routes/partyRoutes.js
+++ server/routes/partyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const protect = require('../middleware/auth');
 const partyController = require('../controllers/partyController');
 
 router.use(protect);
